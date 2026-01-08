573f67ffa9291cdcb2cf9f35248bdaee7219fc3b
Utkrisht K <utkrisht.kumar@tietoevry.com>
2026-01-08 19:18:33 +0530

    Initial Commit

---
+++ src/main/java/com/tietoevry/surest_member_management/SurestMemberManagementApplication.java
@@ -0,0 +1,13 @@
+package com.tietoevry.surest_member_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurestMemberManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurestMemberManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tietoevry/surest_member_management/SurestMemberManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tietoevry.surest_member_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurestMemberManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
